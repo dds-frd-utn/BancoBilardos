b1fa3630859999b32f170bd56d2b54509c61a8d5
fercazenave <cazenavefernando1996@gmail.com>
2019-06-25 19:02:42 -0300

    cache

--- target/generated-sources/annotations/utn/frd/bancobilardos/entities/Cliente_.java
+++ target/generated-sources/annotations/utn/frd/bancobilardos/entities/Cliente_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-06-21T21:20:25")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-06-25T19:00:44")
 @StaticMetamodel(Cliente.class)
 public class Cliente_ { 
 
---
+++ target/generated-sources/annotations/utn/frd/bancobilardos/entities/Cuentas_.java
@@ -0,0 +1,17 @@
+package utn.frd.bancobilardos.entities;
+
+import java.util.Date;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-06-25T19:00:44")
+@StaticMetamodel(Cuentas.class)
+public class Cuentas_ { 
+
+    public static volatile SingularAttribute<Cuentas, Date> fechaBaja;
+    public static volatile SingularAttribute<Cuentas, Date> fechaAlta;
+    public static volatile SingularAttribute<Cuentas, Long> nro;
+    public static volatile SingularAttribute<Cuentas, Long> idCuenta;
+
+}
