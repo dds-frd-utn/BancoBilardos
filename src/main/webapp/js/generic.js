2a55ecd529ddf70935d7c1a10c8fe3bb6a38eae6
fercazenave <cazenavefernando1996@gmail.com>
2019-07-04 20:53:49 -0300

    js crear cliente

--- src/main/webapp/js/generic.js
+++ src/main/webapp/js/generic.js
@@ -1,18 +1,20 @@
+console.log('generic.js');
 function ajax_process ( 
     url, 
     metodo, 
     data,
     callback
 ) {
+    console.log(data);
     if (!data) { data = ''; }
     $.ajax({
-        data: data
+        data: data,
         method:metodo,
         dataType:"json",
         url:url,
         success:callback,
         error:function(error){
-            throw new Exception(error);
+            console.log(error);
         }
     });
 }
